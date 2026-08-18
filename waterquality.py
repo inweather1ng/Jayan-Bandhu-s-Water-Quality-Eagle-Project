69a611189690a798185f32dfc24837c62e398fed
inweather1ng <jayanb661@gmail.com>
2026-08-17 21:37:26 -0700

    Update waterquality.py

--- waterquality.py
+++ waterquality.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Dry Creek Water Quality Monitoring Project")
-tab1,tab2=st.tabs(['Water Chemistry','eDNA sampling")
+tab1,tab2=st.tabs(['Water Chemistry','eDNA sampling'])
 with tab1:
   st.write("site 1, miners ravine")
 
