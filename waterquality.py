3ad14a8cee7a8729236b72375437a2b3a36a5d48
inweather1ng <jayanb661@gmail.com>
2026-08-17 21:02:00 -0700

    Update waterquality.py

--- waterquality.py
+++ waterquality.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title("Dry Creek Water Quality Monitoring Project")
 tab1,tab2=st.tabs(2)
 with tab1:
-st.write("site 1, miners ravine")
+  st.write("site 1, miners ravine")
 
